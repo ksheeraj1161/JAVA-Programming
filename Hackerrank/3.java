01189d701d4eca7373cec945b9626b54518fbcfb
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-07-07 07:52:06 +0530

    Create 3.java

---
+++ Hackerrank/3.java
@@ -0,0 +1,44 @@
+/*
+Input Format
+
+There are three lines of input:
+
+The first line contains an integer.
+The second line contains a double.
+The third line contains a String.
+Output Format
+
+There are three lines of output:
+
+On the first line, print String: followed by the unaltered String read from stdin.
+On the second line, print Double: followed by the unaltered double read from stdin.
+On the third line, print Int: followed by the unaltered integer read from stdin.
+To make the problem easier, a portion of the code is already provided in the editor.
+
+Sample Input
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+
+Sample Output
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();  
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
